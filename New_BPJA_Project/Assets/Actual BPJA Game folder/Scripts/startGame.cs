b5588cf353bbb51883d8153bba6e014b7bbd2ad1
12169407 <70665677+12169407@users.noreply.github.com>
2022-05-17 10:57:20 -0700

    yes

--- New_BPJA_Project/Assets/Actual BPJA Game folder/Scripts/startGame.cs
+++ New_BPJA_Project/Assets/Actual BPJA Game folder/Scripts/startGame.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class startGame : MonoBehaviour
 {
+    GameObject player;
+    GameObject enemySpawner;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemySpawner = GameObject.FindGameObjectWithTag("EnemySpawner");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Player"))
+        if (player.transform.position.z >= 206)
         {
-            GameObject.Find("enemyWaveSpawner").GetComponent<enemyWaveGenerator>().readyToSpawn = true;
+            enemySpawner.GetComponent<enemyWaveGenerator>().readyToSpawn = true;
             Debug.Log("game has begun!!!!");
         }
     }
+    
 }
